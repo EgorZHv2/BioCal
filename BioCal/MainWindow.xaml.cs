18de8cc792f5aeeefcb294d70284b9dfa60c513f
Егор <egor.zholobov@yandex.ru>
2022-06-01 11:36:07 +0300

    Доделал график

--- BioCal/MainWindow.xaml.cs
+++ BioCal/MainWindow.xaml.cs
@@ -2,20 +2,7 @@
 using LiveCharts.Wpf;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace BioCal
 {
@@ -24,30 +11,53 @@ namespace BioCal
     /// </summary>
     public partial class MainWindow : Window
     {
-        List<Stats> stats = new List<Stats>();
-        private SeriesCollection series = new SeriesCollection();
+        private List<Stats> stats = new List<Stats>();
        
         public MainWindow()
         {
             InitializeComponent();
             Dates.ItemsSource = stats;
+
+            chart.AxisY = new AxesCollection()
+            {
+                new Axis()
+                {
+                    Title = "Значения",
+                    MinValue = -1,
+                    MaxValue = 1
+                }
+            };
+            chart.AxisX = new AxesCollection
+                {
+                     new Axis()
+                     {
+                         Title = "Даты",
+                         MinValue = 1
+                         
+                         
+                         
+                         
+                     }
+                };
+
         }
 
-        DateTime birthdate;
-        DateTime startdate;
+        private DateTime birthdate;
+        private DateTime startdate;
+
         private void GenerateGraph_Click(object sender, RoutedEventArgs e)
         {
-            if(Duration.Text == String.Empty || Duration.Text == null)
+            if (Duration.Text == String.Empty || Duration.Text == null)
             {
                 MessageBox.Show("Введите длительность прогноза");
                 e.Handled = true;
             }
-            if(BirthDate.Text == String.Empty || BirthDate.Text == null)
+            if (BirthDate.Text == String.Empty || BirthDate.Text == null)
             {
                 MessageBox.Show("Введите дату рождения");
                 e.Handled = true;
             }
-            if(StartDate.Text == String.Empty || StartDate.Text == null)
+            if (StartDate.Text == String.Empty || StartDate.Text == null)
             {
                 MessageBox.Show("Введите дату отсчёта");
                 e.Handled = true;
@@ -57,43 +67,40 @@ namespace BioCal
             stats.Clear();
             for (int i = 0; i < Convert.ToInt32(Duration.Text); i++)
             {
-                
                 stats.Add(new Stats()
                 {
                     Date = startdate.ToShortDateString(),
-                    Strength = Math.Round(Math.Sin((3.14*2*(startdate-birthdate).Days)/23),4),
-                    Agility = Math.Round(Math.Sin((3.14 * 2 * (startdate - birthdate).Days) / 28),4),
-                    Intelligence = Math.Round(Math.Sin((3.14 * 2 * (startdate - birthdate).Days) / 33),4),
-                    Sum = Math.Round(Math.Sin((3.14 * 2 * (startdate - birthdate).Days) / 23), 4) 
-                    + Math.Round(Math.Sin((3.14 * 2 * (startdate - birthdate).Days) / 28), 4) 
+                    Strength = Math.Round(Math.Sin((3.14 * 2 * (startdate - birthdate).Days) / 23), 4),
+                    Agility = Math.Round(Math.Sin((3.14 * 2 * (startdate - birthdate).Days) / 28), 4),
+                    Intelligence = Math.Round(Math.Sin((3.14 * 2 * (startdate - birthdate).Days) / 33), 4),
+                    Sum = Math.Round(Math.Sin((3.14 * 2 * (startdate - birthdate).Days) / 23), 4)
+                    + Math.Round(Math.Sin((3.14 * 2 * (startdate - birthdate).Days) / 28), 4)
                     + Math.Round(Math.Sin((3.14 * 2 * (startdate - birthdate).Days) / 33), 4)
-
                 });
                 startdate = startdate.AddDays(1);
             }
             Dates.Items.Refresh();
-            double maxstrenght = double.MinValue;           
+            double maxstrenght = double.MinValue;
             double maxagility = double.MinValue;
             double maxintelligence = double.MinValue;
             double maxsum = double.MinValue;
             StatBirthDate.Text = "Дата рождения - " + birthdate.ToShortDateString();
             StatDuration.Text = "Длительность прогноза - " + Duration.Text;
             StatPeriod.Text = "Период с " + stats[0].Date + " по " + stats[stats.Count - 1].Date;
-            
-            foreach(Stats stat in stats)
+
+            foreach (Stats stat in stats)
             {
-                if(stat.Strength > maxstrenght)
+                if (stat.Strength > maxstrenght)
                 {
                     maxstrenght = stat.Strength;
                     StatMaxStrenght.Text = $"Физические максимумы ({maxstrenght}): " + stat.Date;
-                    
                 }
-                if(stat.Agility > maxagility)
+                if (stat.Agility > maxagility)
                 {
                     maxagility = stat.Agility;
                     StatMaxAgility.Text = $"Эмоциональные максимумы ({maxagility}): " + stat.Date;
                 }
-                if(stat.Intelligence > maxintelligence)
+                if (stat.Intelligence > maxintelligence)
                 {
                     maxintelligence = stat.Intelligence;
                     StatMaxIntelligence.Text = $"Интеллектуальные максимумы ({maxintelligence}): " + stat.Date;
@@ -102,16 +109,21 @@ namespace BioCal
                 {
                     maxsum = stat.Sum;
                     StatMaxSum.Text = $"Суммарные максимумы ({maxsum}): " + stat.Date;
-                }                   
+                }
             }
+            SeriesCollection series = new SeriesCollection();
+            chart.Series = series;
             ChartValues<double> StrengthValues = new ChartValues<double>();
             ChartValues<double> AgilityValues = new ChartValues<double>();
             ChartValues<double> IntelligenceValues = new ChartValues<double>();
-            foreach(Stats stat in stats)
+            List<string> ChartDates = new List<string>();
+
+            foreach (Stats stat in stats)
             {
                 StrengthValues.Add(stat.Strength);
                 AgilityValues.Add(stat.Agility);
                 IntelligenceValues.Add(stat.Intelligence);
+                ChartDates.Add(stat.Date);
             }
 
             series.Add(new LineSeries
@@ -129,15 +141,35 @@ namespace BioCal
                 Title = "Интеллектуальные ритмы",
                 Values = IntelligenceValues
             });
-            
-            chart.Series = series;
-            chart.Update();
 
+            if (DateRequired.IsChecked == true)
+            {
+                chart.AxisX = new AxesCollection
+                {
+                     new Axis()
+                     {
+                         Title = "Даты",
+                         Labels = ChartDates
+                     }
+                };
+            }
+            else
+            {
+                chart.AxisX = new AxesCollection
+                {
+                     new Axis()
+                     {
+                         Title = "Даты",
+                         MinValue = 0
+                     }
+                };
+            }
+
+            chart.Update();
         }
 
         private void ExportToWord_Click(object sender, RoutedEventArgs e)
         {
-
         }
     }
 }
--- BioCal/Stats.cs
+++ BioCal/Stats.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BioCal
+﻿namespace BioCal
 {
     public class Stats
     {
-      public string Date { get; set; }
-      public double Strength { get; set; }
-      public double Agility { get; set; }
-      public double Intelligence { get; set; }
-      public double Sum { get; set; }
+        public string Date { get; set; }
+        public double Strength { get; set; }
+        public double Agility { get; set; }
+        public double Intelligence { get; set; }
+        public double Sum { get; set; }
     }
 }
