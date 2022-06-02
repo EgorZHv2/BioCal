38a5e0be3d2b888ce5359b7665c1b2123cd3dfe0
Егор <egor.zholobov@yandex.ru>
2022-06-02 12:52:51 +0300

    Добавлен класс Bio

---
+++ BioCal/Bio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BioCal
+{
+    public class Bio
+    {
+        public List<Stats> GenerateList(DateTime birthdate,DateTime starttime,int duration)
+        {
+            return new List<Stats>();
+        }
+    }
+}
